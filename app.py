a3227f037b9eea4910bd8159f6a9f09507506fbc
Sagar S <sagarpopzine@gmail.com>
2025-12-10 19:43:00 +0530

    Fix merge conflict in app.py
    Removed merge conflict markers from app.py.

--- app.py
+++ app.py
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
- 
->>>>>>> 3d54099f68df1e94bfdb21b1bcf8618775e51f35
 from flask import Flask
 import os
 app = Flask(__name__)
@@ -16,7 +12,7 @@ def hell():
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
-<<<<<<< HEAD
-=======
+
+
  
->>>>>>> 3d54099f68df1e94bfdb21b1bcf8618775e51f35
+
